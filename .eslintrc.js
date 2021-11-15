029011be25be5a9acfdf4e451aa24cb97be669c0
David Amaral Silva <david_silva_94@live.com.pt>
2021-11-15 18:52:22 +0000

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+        "react/no-unescaped-entities": 0,
+        'react/prop-types': 0,
+  },
+};
